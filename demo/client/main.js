998fe076ea0c22cbab78bf87e99eb808700ca2d8
PEM-- <pemarchandet@gmail.com>
2017-02-04 15:57:53 +0100

    Use React for the demo

--- demo/client/main.js
+++ demo/client/main.js
@@ -1,22 +1,14 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Meteor } from 'meteor/meteor';
 
-import './main.html';
+const App = () => (
+  <main>
+    <h1>Demo</h1>
+    <p>Simple demo</p>
+  </main>
+)
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
-
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
-
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
+Meteor.startup(() => {
+  ReactDOM.render(<App />, document.getElementById('react-root'));
 });
